e6e002e40c49a7ab3d87e9dcb43d530a95d8c8c8
Your Name <you@example.com>
2025-02-21 22:24:24 +0200

    removing variables and adding calculated fields

--- NS.py
+++ NS.py
@@ -10,13 +10,18 @@ def manage_finances():
         "rent_percent": float(input("Enter the percentage for rent: ")),
         "electricity_percent": float(input("Enter the percentage for electricity: "))
     }
-    salary=finance_data[month]["salary"]
-    savingPercent=finance_data[month]["savings_percent"]
-    rentPercent=finance_data[month]["rent_percent"]
-    electricity=finance_data[month]["electricity_percent"]
+    #variables removed
+   # salary=finance_data[month]["salary"]
+    #savingPercent=finance_data[month]["savings_percent"]
+    #rentPercent=finance_data[month]["rent_percent"]
+    #electricity=finance_data[month]["electricity_percent"]
 
-
-
+    #calculate allocation(from percent supplied)
+    savings=(finance_data[month]["savings_percent"]/100)*finance_data[month]["salary"]
+    rent=(finance_data[month]["rent_percent"]/100)*finance_data[month]["salary"]
+    electricity=(finance_data[month]["electricity_percent"]/100)*finance_data[month]["salary"]
+    totalExpenses=savings+rent+electricity
+    remaining=finance_data[month]["salary"]-totalExpenses
 
 # Run the function
 manage_finances()
